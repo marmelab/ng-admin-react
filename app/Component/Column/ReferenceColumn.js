dcbf1b408ae01afc91360309d5f7840881cf3f60
Emmanuel QUENTIN <manu.quentin@gmail.com>
2015-06-08 10:59:58 +0200

    Add link in reference column

--- app/Component/Column/ReferenceColumn.js
+++ app/Component/Column/ReferenceColumn.js
@@ -1,16 +1,29 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 class ReferenceColumn extends React.Component {
     render() {
-        var value = this.props.value;
+        let {value, field, entry} = this.props,
+            referenceEntity = field.targetEntity(),
+            to = referenceEntity.isReadOnly ? 'show' : field.detailLinkRoute(),
+            referenceId = entry.values[field.name()],
+            params = {
+                entity: referenceEntity.name(),
+                id: referenceId
+            };
+
         return (
-            <span>{value}</span>
+            <Link to={to} params={params}>
+                {value}
+            </Link>
         );
     }
 }
 
 ReferenceColumn.propTypes = {
-    value: React.PropTypes.string
+    value: React.PropTypes.string,
+    entry: React.PropTypes.object,
+    field: React.PropTypes.object
 };
 
 export default ReferenceColumn;
--- app/Component/Datagrid/Datagrid.js
+++ app/Component/Datagrid/Datagrid.js
@@ -75,7 +75,7 @@ class Datagrid extends React.Component {
                     break;
 
                 case 'reference':
-                    renderedField = <ReferenceColumn value={row.listValues[fieldName]} />;
+                    renderedField = <ReferenceColumn value={row.listValues[fieldName]} field={field} entry={row} />;
                     break;
 
                 case 'reference_many':

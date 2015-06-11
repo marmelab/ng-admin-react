00b9358f83ff50daf48ccc06d9d02e6514b707b6
Emmanuel QUENTIN <manu.quentin@gmail.com>
2015-06-11 10:11:43 +0200

    Code review

--- jest-bootstrap.js
+++
@@ -1,8 +0,0 @@
-var originalWarn = console.warn;
-console.warn = function (message) {
-    if (message.indexOf('Don\'t set .props') !== -1) {
-        return;
-    }
-
-    originalWarn(message);
-};

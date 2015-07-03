79bf637c4d29240066ca41178a9a12885fdd5f2b
Jérôme Macias <jerome.macias@gmail.com>
2015-07-03 23:25:37 +0200

    Add unit tests for Filters and FilterButton components

--- app/Component/Datagrid/__tests__/FiltersTest.js
+++ app/Component/Datagrid/__tests__/FiltersTest.js
@@ -3,7 +3,7 @@ jest.autoMockOff();
 describe('Filters', () => {
     const Immutable = require('immutable');
     const React = require('react/addons');
-    //const TestUtils = React.addons.TestUtils;
+    const TestUtils = React.addons.TestUtils;
     const routerWrapper = require('../../../Test/RouterWrapper');
 
     const Field = require('admin-config/lib/Field/Field');
@@ -22,7 +22,9 @@ describe('Filters', () => {
 
     let hidden = [];
     const hideFilter = (filter) => {
-        hidden.push(filter.name);
+        return () => {
+            hidden.push(filter.name());
+        };
     };
 
     let updated = {};
@@ -35,17 +37,53 @@ describe('Filters', () => {
     }
 
     describe('Display', () => {
-        it('should display selected filters', () => {
+        it('should display pinned and selected filters', () => {
             const selectedFilters = Immutable.List([pinnedFilter, notPinned]);
             let filters = getFilters(selectedFilters);
             filters = React.findDOMNode(filters);
 
-            const field = filters.querySelector('input');
-            const label = filters.querySelector('label');
+            const pinnedField = filters.querySelector('.filter-author input');
+            const pinnedLabel = filters.querySelector('.filter-author label');
+            const pinnedRemove = filters.querySelector('.filter-author a.remove');
 
-            expect(field.attributes.name.value).toEqual('author');
-            expect(field.attributes.type.value).toEqual('text');
-            expect(label.innerHTML).toEqual('Author');
+            expect(pinnedField.attributes.name.value).toEqual('author');
+            expect(pinnedField.attributes.type.value).toEqual('text');
+            expect(pinnedLabel.innerHTML).toEqual('Author');
+            expect(pinnedRemove).toEqual(null);
+
+            const notPinnedField = filters.querySelector('.filter-name input');
+            const notPinnedLabel = filters.querySelector('.filter-name label');
+            const notPinnedRemove = filters.querySelector('.filter-name a.remove');
+
+            expect(notPinnedField.attributes.name.value).toEqual('name');
+            expect(notPinnedLabel.innerHTML).toEqual('Name');
+            expect(notPinnedRemove.innerHTML).toContain('glyphicon-remove');
+        });
+    });
+
+    describe('Callback', () => {
+        it('should call hideFilter callback on filter remove', () => {
+            const selectedFilters = Immutable.List([pinnedFilter, notPinned]);
+            let filters = getFilters(selectedFilters);
+            filters = React.findDOMNode(filters);
+
+            const notPinnedRemove = filters.querySelector('.filter-name a.remove');
+
+            TestUtils.Simulate.click(notPinnedRemove);
+
+            expect(hidden).toEqual(['name']);
+        });
+
+        it('should call updateField callback on filter value changed', () => {
+            const selectedFilters = Immutable.List([pinnedFilter, notPinned]);
+            let filters = getFilters(selectedFilters);
+            filters = React.findDOMNode(filters);
+
+            const notPinnedField = filters.querySelector('.filter-name input');
+
+            TestUtils.Simulate.change(notPinnedField, { target: { value: 'Me'} });
+
+            expect(updated).toEqual({ name: 'Me' });
         });
     });
 });

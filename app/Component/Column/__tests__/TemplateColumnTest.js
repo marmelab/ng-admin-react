ca8c43a1585202b605160fd1507dba3a0542d86b
Jérôme Macias <jerome.macias@gmail.com>
2015-07-06 12:50:44 +0200

    Reenable TemplateColumn test with Compile usage

--- app/Component/Column/__tests__/TemplateColumnTest.js
+++ app/Component/Column/__tests__/TemplateColumnTest.js
@@ -6,8 +6,8 @@ describe('TemplateColumn', () => {
     const TemplateColumn = require('../TemplateColumn');
 
     it('should execute template function with current entry if is a function', () => {
-        const template = (e => e.first_name + " " + e.last_name.toUpperCase());
-        const entry = { last_name: 'Doe', first_name: 'John' };
+        const template = e => e.first_name + ' ' + e.last_name.toUpperCase();
+        const entry = { 'last_name': 'Doe', 'first_name': 'John' };
 
         let templateColumn = TestUtils.renderIntoDocument(<TemplateColumn template={template} entry={entry} />);
         templateColumn = React.findDOMNode(templateColumn);
@@ -15,9 +15,9 @@ describe('TemplateColumn', () => {
         expect(templateColumn.textContent).toBe('John DOE');
     });
 
-    xit('should transform template string through React compiler, giving access to `entry` value', () => {
-        const template = "{e.first_name} {e.last_name.toUpperCase()}";
-        const entry = { last_name: 'Doe', first_name: 'John' };
+    it('should transform template string through React compiler, giving access to `entry` value', () => {
+        const template = '{entry.first_name} {entry.last_name.toUpperCase()}';
+        const entry = { 'last_name': 'Doe', 'first_name': 'John' };
 
         let templateColumn = TestUtils.renderIntoDocument(<TemplateColumn template={template} entry={entry} />);
         templateColumn = React.findDOMNode(templateColumn);
